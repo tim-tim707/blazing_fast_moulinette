7de293a4b017bb8a82cbff9028a9aeab05be0017
Timothee Denizou <timothee.denizou@epita.fr>
2021-09-14 13:28:57 +0200

    Added commit message fiel generator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     env,
-    fs::{create_dir_all, File},
+    fs::{create_dir_all, File, OpenOptions},
     io::{prelude::*, BufReader},
     path::Path,
     process::Command,
@@ -19,8 +19,8 @@ fn clone_all(student_list_filename: &String, practical_number: &String) {
     let student_list = lines_from_file(student_list_filename);
 
     println!("Cloning...");
-    for line in student_list {
-        let student_dir = format!("tp{}-{}", practical_number, line);
+    for student in student_list {
+        let student_dir = format!("tp{}-{}", practical_number, student);
         let cloning_dir = format!("../TP{}/{}", practical_number, student_dir);
         create_dir_all(&cloning_dir).expect("Directory already exist");
         let repo = format!("git@git.cri.epita.fr:p/2025-s3-tp/{}", student_dir);
@@ -36,6 +36,41 @@ fn clone_all(student_list_filename: &String, practical_number: &String) {
     println!("Cloning done");
 }
 
+fn commit_messages(student_list_filename: &String, practical_number: &String) {
+    let commit_message_file = format!("../TP{}/commit_message.txt", practical_number);
+    let commit_message_path = Path::new(&commit_message_file);
+
+    if Path::exists(commit_message_path) {
+        eprintln!("Warning: commit file already exists, no modification will occur");
+    } else {
+        let mut commit_file = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(commit_message_path)
+            .expect("Couldnt create commit file");
+
+        let student_list = lines_from_file(student_list_filename);
+        for student in student_list {
+            let student_dir = format!("tp{}-{}", practical_number, student);
+            let cloning_dir = format!("../TP{}/{}", practical_number, student_dir);
+
+            let command_result = Command::new("git")
+                .current_dir(cloning_dir)
+                .args(["log", "--pretty=format:%s"])
+                .output()
+                .expect("Couldnt get commit messages");
+
+            let commit_messages = String::from_utf8_lossy(&command_result.stdout);
+
+            //File::write_all(&mut commit_file, format!("\n\n{}\n", student).as_bytes()).expect("Couldnt write student name");
+            File::write_all(&mut commit_file, "\n\n====\n".as_bytes())
+                .expect("Couldnt write student separator");
+            File::write_all(&mut commit_file, commit_messages.as_bytes())
+                .expect("Couldnt write commit messages");
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -43,5 +78,6 @@ fn main() {
         panic!("Please provide the student list and then the number of the practical");
     }
 
-    clone_all(&args[2], &args[1])
+    //clone_all(&args[2], &args[1])
+    commit_messages(&args[1], &args[2]);
 }
