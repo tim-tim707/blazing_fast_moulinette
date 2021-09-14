a01024ce9904806828492966ae5de205c429f6af
Timothee Denizou <timothee.denizou@epita.fr>
2021-09-14 19:31:03 +0200

    refactored code by adding structures

--- src/main.rs
+++ src/main.rs
@@ -15,20 +15,51 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
-fn clone_all(student_list_filename: &String, practical_number: &String) {
-    let student_list = lines_from_file(student_list_filename);
+struct PracticalInfo {
+    practical_path: String,
+    student_list: Vec<StudentInfo>,
+}
+struct StudentInfo {
+    login: String,
+    practical_dir: String,
+}
 
+impl PracticalInfo {
+    fn create_practical_data(student_list: &String, practical_number: &String) -> PracticalInfo {
+        PracticalInfo {
+            practical_path: format!("../TP{}", practical_number),
+            student_list: lines_from_file(student_list)
+                .iter()
+                .map(|login| StudentInfo {
+                    login: login.to_string(),
+                    practical_dir: format!("tp{}-{}", practical_number, login),
+                })
+                .collect(),
+        }
+    }
+}
+
+fn clone_all(infos: &PracticalInfo) {
     println!("Cloning...");
-    for student in student_list {
-        let student_dir = format!("tp{}-{}", practical_number, student);
-        let cloning_dir = format!("../TP{}/{}", practical_number, student_dir);
-        create_dir_all(&cloning_dir).expect("Directory already exist");
-        let repo = format!("git@git.cri.epita.fr:p/2025-s3-tp/{}", student_dir);
+    for student in &infos.student_list {
+        create_dir_all(&format!(
+            "{}/{}",
+            infos.practical_path, student.practical_dir
+        ))
+        .expect("Directory already exist");
 
         Command::new("git")
-            .arg("clone")
-            .arg(repo)
-            .arg(cloning_dir)
+            .current_dir(&format!(
+                "{}/{}",
+                infos.practical_path, student.practical_dir
+            ))
+            .args([
+                "clone",
+                &format!(
+                    "git@git.cri.epita.fr:p/2025-s3-tp/{}",
+                    student.practical_dir
+                ),
+            ])
             .output()
             .expect("Couldnt clone");
     }
@@ -36,9 +67,9 @@ fn clone_all(student_list_filename: &String, practical_number: &String) {
     println!("Cloning done");
 }
 
-fn commit_messages(student_list_filename: &String, practical_number: &String) {
-    let commit_message_file = format!("../TP{}/commit_message.txt", practical_number);
-    let commit_message_path = Path::new(&commit_message_file);
+fn commit_messages(infos: &PracticalInfo) {
+    let commit_message = &format!("{}/commit_message.txt", infos.practical_path);
+    let commit_message_path = Path::new(commit_message);
 
     if Path::exists(commit_message_path) {
         eprintln!("Warning: commit file already exists, no modification will occur");
@@ -49,22 +80,24 @@ fn commit_messages(student_list_filename: &String, practical_number: &String) {
             .open(commit_message_path)
             .expect("Couldnt create commit file");
 
-        let student_list = lines_from_file(student_list_filename);
-        for student in student_list {
-            let student_dir = format!("tp{}-{}", practical_number, student);
-            let cloning_dir = format!("../TP{}/{}", practical_number, student_dir);
-
+        for student in &infos.student_list {
             let command_result = Command::new("git")
-                .current_dir(cloning_dir)
+                .current_dir(&format!(
+                    "{}/{}",
+                    infos.practical_path, student.practical_dir
+                ))
                 .args(["log", "--pretty=format:%s"])
                 .output()
                 .expect("Couldnt get commit messages");
 
             let commit_messages = String::from_utf8_lossy(&command_result.stdout);
 
-            //File::write_all(&mut commit_file, format!("\n\n{}\n", student).as_bytes()).expect("Couldnt write student name");
-            File::write_all(&mut commit_file, "\n\n====\n".as_bytes())
-                .expect("Couldnt write student separator");
+            File::write_all(
+                &mut commit_file,
+                format!("\n\n{}\n", student.login).as_bytes(),
+            )
+            .expect("Couldnt write student name");
+            //File::write_all(&mut commit_file, "\n\n====\n".as_bytes()).expect("Couldnt write student separator");
             File::write_all(&mut commit_file, commit_messages.as_bytes())
                 .expect("Couldnt write commit messages");
         }
@@ -78,6 +111,7 @@ fn main() {
         panic!("Please provide the student list and then the number of the practical");
     }
 
-    //clone_all(&args[2], &args[1])
-    commit_messages(&args[1], &args[2]);
+    let infos: PracticalInfo = PracticalInfo::create_practical_data(&args[1], &args[2]);
+    clone_all(&infos);
+    commit_messages(&infos);
 }
